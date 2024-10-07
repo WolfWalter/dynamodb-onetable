051960c8bb04e132e73f296c1c88f86e23a502aa
Wolf Walter <wwalter@solvis.de>
2024-10-07 16:34:09 +0200

    🧪 demonstrating update issues
    issue 1: set sub property to undefined/null
    issue 2: updating date field with set

--- test/debug.ts
+++ test/debug.ts
@@ -5,8 +5,7 @@
 
     Or run VS Code in the top level directory and just run.
  */
-import {AWS, Client, Entity, Match, Model, Table, print, dump, delay} from './utils/init'
-import {OneSchema} from '../src/index.js'
+import { Client, Table } from './utils/init'
 
 jest.setTimeout(7200 * 1000)
 
@@ -30,6 +29,16 @@ const schema = {
             pk: { type: String, value: 'user#' },
             sk: { type: String, value: 'user#${email}' },
             email: { type: String, required: true },
+            address: {
+              type: Object,
+              default: {},
+              schema: {
+                  street: {type: String},
+                  city: {type: String},
+                  zip: {type: String},
+              },
+            },
+            updatedAt: {type: Date },
         }
     } as const,
 }
@@ -51,17 +60,26 @@ test('Create Table', async () => {
     }
 })
 
-test('Test', async () => {
-    /*
-        Put your code here
+test('Update sub property to undefined/null should not trow', async () => {
+  let User = table.getModel('User')
 
-    let User = table.getModel('User')
-    let user = await User.create({
-        email: "user@example.com",
-    })
-    dump("USER", user)
-    let result = await User.find({}, {log: true})
-    */
+  // throws OneTableError: OneTable execute failed "update" for "User", The conditional request failed
+  // because it tries to remove the `zip` and set the `address` in the same update
+  await expect(
+    () => User.update({email: 'roadrunner@acme.com', address: { street: 'aStreet', city: 'aCity', zip: undefined }})
+  ).rejects.not.toThrow()
+})
+
+test('Update date field with set should not throw', async () => {
+  let User = table.getModel('User')
+
+  // throws: Unsupported type passed: Mon Oct 07 2024 16:21:29 GMT+0200 (Central European Summer Time). Pass options.convertClassInstanceToMap=true to marshall typeof object as map attribute. 
+  await expect(
+    () => User.update({email: 'roadrunner@acme.com'},{ set: { updatedAt: new Date() }})
+  ).rejects.not.toThrow()
+
+
+  // if setting options.convertClassInstanceToMap=true as suggested by the error it gets worse and the date gets set to `Invalid Date`for the local DDB and an empty object/DDB-Map in a real DDB
 })
 
 test('Destroy Table', async () => {
